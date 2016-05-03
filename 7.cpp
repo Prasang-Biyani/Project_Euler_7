6058dd8dc6558f297802bf99e3a5283f741c5ff3
Prasang13 <biyaniprasang@gmail.com>
2016-05-03 10:50:09 +0530

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -4,19 +4,19 @@ using namespace std;
 static const unsigned long long int N=190000;
 int main()
 {
-    int prime_num = 10001;
+    int prime_num = 10001;                      // nth number whose prime to found (here n=10001)
     unsigned long long int i,j,num[N];
     for(i=2;i<N;i++)
-        num[i]=1;
+        num[i]=1;                                   // considering all number are primes;
     for(i=2;i<sqrt(N);i++)
     {
         if(num[i]) {
         for(j=i;j*i<N;j++) {
-            num[i*j]=0;
+            num[i*j]=0;                             // clearing out the multiples of i*j;
             }
         }
     }
-    int count = 0;
+    int count = 0;                                  //intializing the count to zero upto prime_num.
     for(i=2;i<N;i++) {
         if(num[i]) {
             count++;
